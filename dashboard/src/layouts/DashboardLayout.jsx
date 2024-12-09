af91af90c966c58910b3c249e5cd87deb17eab66
Boluwatife <boluwatife david>
2024-12-09 08:21:51 +0100

    animation for dashboard

--- dashboard/src/layouts/DashboardLayout.jsx
+++ dashboard/src/layouts/DashboardLayout.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { motion } from "framer-motion";
 
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
@@ -7,15 +8,21 @@ export default function DashboardLayout() {
   
   return (
     <div className="flex flex-col md:flex-row h-screen w-full bg-gray-100">
-        <div className="sidebar-container md:w-96 h-screen w-full bg-gray-800">
+        <div className="sidebar-container md:w-96 h-screen w-full">
         {/* Sidebar */}
             <Sidebar />
         </div>
 
         {/* Main Content Area */}
-        <div className="flex-1 bg-gray-100 p-6 h-full md:overflow-y-auto">
+        <motion.div 
+          className="flex-1 bg-gray-100 p-6 h-full md:overflow-y-auto"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          >
             <Outlet />
-        </div>
+        </motion.div>
     </div>
   )
 }
